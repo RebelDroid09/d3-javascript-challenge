616d8f89684ff3ad6e095e516a0c7cfc084e06f0
RebelDroid09 <paulcs91@gmail.com>
2021-01-06 17:44:32 -0600

    Update app.js
    Added initial code to load data set.

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,1 +1,26 @@
 // @TODO: YOUR CODE HERE!
+var  dataset;
+
+d3.json("./assets/data/data.json").then(function(data)
+{ 
+    dataset = data;
+
+    console.log(data);
+
+    subjectNames = dataset.names;
+
+    var dropdown = d3.select("#selDataset");
+
+    var subjectOptions = dropdown.selectAll('option')
+        .data(subjectNames)
+        .enter()
+        .append("option")
+        .attr("value", function(d) {
+            return d;
+        })
+        .text(function(d) {
+            return d;
+        }); 
+
+    updatePlotly();
+});
